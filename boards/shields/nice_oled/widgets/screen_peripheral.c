186c81a32a5405ed0faafb40bf14834066233637
Valkride <venzovossen@outlook.com>
2025-06-10 21:36:13 +0200

    modified:   boards/shields/nice_oled/widgets/output.h 	modified:   boards/shields/nice_oled/widgets/screen_peripheral.c

--- boards/shields/nice_oled/widgets/screen_peripheral.c
+++ boards/shields/nice_oled/widgets/screen_peripheral.c
@@ -17,6 +17,7 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #include "battery.h"
 #include "layer.h"
 #include "output.h"
+#include "profile.h"
 #include "screen_peripheral.h"
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
