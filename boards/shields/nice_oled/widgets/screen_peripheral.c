989ced0aae4092060c556cf3f73676838706b2ad
Valkride <venzovossen@outlook.com>
2025-06-12 21:41:34 +0200

    modified:   boards/shields/nice_oled/widgets/screen_peripheral.c

--- boards/shields/nice_oled/widgets/screen_peripheral.c
+++ boards/shields/nice_oled/widgets/screen_peripheral.c
@@ -47,6 +47,11 @@ static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 static void draw_canvas(lv_obj_t *widget, lv_color_t cbuf[], const struct status_state *state) {
     lv_obj_t *canvas = lv_obj_get_child(widget, 0);
 
+    // Draw static debug text at boot to confirm OLED works
+    lv_draw_label_dsc_t label_dsc;
+    init_label_dsc(&label_dsc, LVGL_FOREGROUND, &pixel_operator_mono_16, LV_TEXT_ALIGN_CENTER);
+    lv_canvas_draw_text(canvas, 0, 0, CANVAS_WIDTH, &label_dsc, "HELLO", LV_LABEL_ALIGN_CENTER);
+
     // Draw widgets
     draw_wpm_status(canvas, state);
     draw_layer_status(canvas, state);
