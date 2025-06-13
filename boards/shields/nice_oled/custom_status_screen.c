3a789ffa39be0cea375868e7fc626ecc594de129
Valkride <venzovossen@outlook.com>
2025-06-13 21:46:29 +0200

    modified:   boards/shields/nice_oled/custom_status_screen.c

--- boards/shields/nice_oled/custom_status_screen.c
+++ boards/shields/nice_oled/custom_status_screen.c
@@ -1,4 +1,5 @@
 #include "widgets/screen.h"
+#include "widgets/screen_peripheral.h"
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
@@ -12,6 +13,7 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #if IS_ENABLED(CONFIG_NICE_VIEW_WIDGET_STATUS)
 static struct zmk_widget_screen screen_widget;
+static struct zmk_widget_screen_peripheral screen_peripheral_widget;
 #endif
 
 lv_obj_t *zmk_display_status_screen() {
@@ -21,6 +23,8 @@ lv_obj_t *zmk_display_status_screen() {
 #if IS_ENABLED(CONFIG_NICE_VIEW_WIDGET_STATUS)
     zmk_widget_screen_init(&screen_widget, screen);
     lv_obj_align(zmk_widget_screen_obj(&screen_widget), LV_ALIGN_TOP_LEFT, 0, 0);
+    zmk_widget_screen_peripheral_init(&screen_peripheral_widget, screen);
+    lv_obj_align(zmk_widget_screen_peripheral_obj(&screen_peripheral_widget), LV_ALIGN_TOP_LEFT, 0, 0);
 #endif
 
     return screen;
