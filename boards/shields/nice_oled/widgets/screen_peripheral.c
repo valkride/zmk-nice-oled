ada3d35f46274f7884fa6537487da8e73138cc5d
Valkride <venzovossen@outlook.com>
2025-06-15 01:44:29 +0200

    modified:   boards/shields/nice_oled/widgets/screen_peripheral.c

--- boards/shields/nice_oled/widgets/screen_peripheral.c
+++ boards/shields/nice_oled/widgets/screen_peripheral.c
@@ -13,7 +13,6 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #include "profile.h"
 #include "output.h"
 #include "screen_peripheral.h"
-#include "split_sync.h"
 #include "../assets/custom_fonts.h" // Add this line to declare pixel_operator_mono_16
 
 // Fallback macros for event helpers only
