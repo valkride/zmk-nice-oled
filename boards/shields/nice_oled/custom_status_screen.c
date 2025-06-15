afd687e405af40c52b9b16160b8039227cef0edc
Valkride <venzovossen@outlook.com>
2025-06-15 16:00:25 +0200

    modified:   boards/shields/nice_oled/custom_status_screen.c

--- boards/shields/nice_oled/custom_status_screen.c
+++ boards/shields/nice_oled/custom_status_screen.c
@@ -21,10 +21,15 @@ lv_obj_t *zmk_display_status_screen() {
     screen = lv_obj_create(NULL);
 
 #if IS_ENABLED(CONFIG_NICE_VIEW_WIDGET_STATUS)
+    #if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
+    // Main/Central keyboard: show only the simple activity bar (Bluetooth icon + battery)
     zmk_widget_screen_init(&screen_widget, screen);
     lv_obj_align(zmk_widget_screen_obj(&screen_widget), LV_ALIGN_TOP_LEFT, 0, 0);
+    #else
+    // Peripheral keyboard: show all meters (WPM, layer, profile)
     zmk_widget_screen_peripheral_init(&screen_peripheral_widget, screen);
     lv_obj_align(zmk_widget_screen_peripheral_obj(&screen_peripheral_widget), LV_ALIGN_TOP_LEFT, 0, 0);
+    #endif
 #endif
 
     return screen;
