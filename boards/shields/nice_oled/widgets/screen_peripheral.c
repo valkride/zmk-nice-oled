f4636671484bf6aa90f5f31d403146fb148370f6
Valkride <venzovossen@outlook.com>
2025-06-15 15:15:16 +0200

    modified:   boards/shields/nice_oled/widgets/screen_peripheral.c

--- boards/shields/nice_oled/widgets/screen_peripheral.c
+++ boards/shields/nice_oled/widgets/screen_peripheral.c
@@ -7,16 +7,10 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #include <zmk/events/wpm_state_changed.h>
 #include <zmk/events/layer_state_changed.h>
 #include <zmk/events/ble_active_profile_changed.h>
-#include <zmk/events/endpoint_changed.h>
-#include <zmk/events/usb_conn_state_changed.h>
-#include <zmk/ble.h>
-#include <zmk/endpoints.h>
-#include <zmk/usb.h>
 
 #include "wpm.h"
 #include "layer.h"
 #include "profile.h"
-#include "output.h"
 #include "screen_peripheral.h"
 #include "../assets/custom_fonts.h" // Add this line to declare pixel_operator_mono_16
 
@@ -52,11 +46,11 @@ static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 static void draw_canvas(lv_obj_t *widget, lv_color_t cbuf[], const struct status_state *state) {
     lv_obj_t *canvas = lv_obj_get_child(widget, 0);
     lv_canvas_fill_bg(canvas, LVGL_BACKGROUND, LV_OPA_COVER);
-    // Peripheral OLED: show all meters, stacked, ZMK style with more spacing
+    // Peripheral OLED: show WPM, layer, and profile meters only (no output status)
     draw_wpm_status(canvas, state, 0);      // Top (WPM gauge + graph takes ~35px)
     draw_layer_status(canvas, state, 35);   // Below WPM 
     draw_profile_status(canvas, state, 50); // Below Layer (text + bar takes ~15px)
-    draw_output_status(canvas, state, 70);  // Bottom (just text, takes ~15px)
+    // Note: No output status on peripheral since it doesn't have endpoint info
     rotate_canvas(canvas, cbuf);
 }
 
@@ -125,40 +119,6 @@ ZMK_DISPLAY_WIDGET_LISTENER(widget_profile_status, struct profile_status_state, 
 ZMK_SUBSCRIPTION(widget_profile_status, zmk_ble_active_profile_changed);
 
 /**
- * Output status (for bluetooth/USB icons)
- **/
-static void set_output_status(struct zmk_widget_screen_peripheral *widget, struct output_status_state state) {
-    widget->state.selected_endpoint = state.selected_endpoint;
-    widget->state.active_profile_index = state.active_profile_index;
-    widget->state.active_profile_connected = state.active_profile_connected;
-    widget->state.active_profile_bonded = state.active_profile_bonded;
-    draw_canvas(widget->obj, widget->cbuf, &widget->state);
-}
-
-static void output_status_update_cb(struct output_status_state state) {
-    struct zmk_widget_screen_peripheral *widget;
-    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) { set_output_status(widget, state); }
-}
-
-static struct output_status_state output_status_get_state(const zmk_event_t *eh) {
-    return (struct output_status_state){
-        .selected_endpoint = zmk_endpoints_selected(),
-        .active_profile_index = zmk_ble_active_profile_index(),
-        .active_profile_connected = zmk_ble_active_profile_is_connected(),
-        .active_profile_bonded = !zmk_ble_active_profile_is_open(),
-    };
-}
-
-ZMK_DISPLAY_WIDGET_LISTENER(widget_output_status, struct output_status_state, output_status_update_cb, output_status_get_state)
-ZMK_SUBSCRIPTION(widget_output_status, zmk_endpoint_changed);
-#if IS_ENABLED(CONFIG_USB_DEVICE_STACK)
-ZMK_SUBSCRIPTION(widget_output_status, zmk_usb_conn_state_changed);
-#endif
-#if defined(CONFIG_ZMK_BLE)
-ZMK_SUBSCRIPTION(widget_output_status, zmk_ble_active_profile_changed);
-#endif
-
-/**
  * Initialization
  **/
 int zmk_widget_screen_peripheral_init(struct zmk_widget_screen_peripheral *widget, lv_obj_t *parent) {
@@ -173,7 +133,6 @@ int zmk_widget_screen_peripheral_init(struct zmk_widget_screen_peripheral *widge
     widget_wpm_status_init();
     widget_layer_status_init();
     widget_profile_status_init();
-    widget_output_status_init();
 
     return 0;
 }
