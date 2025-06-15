5c5e9ea86036a5a1263198608ea14850ca55fe67
Valkride <venzovossen@outlook.com>
2025-06-15 15:26:11 +0200

    modified:   boards/shields/nice_oled/widgets/screen.c

--- boards/shields/nice_oled/widgets/screen.c
+++ boards/shields/nice_oled/widgets/screen.c
@@ -9,15 +9,48 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #include <zmk/events/battery_state_changed.h>
 #include <zmk/events/split_peripheral_status_changed.h>
 #include <zmk/events/usb_conn_state_changed.h>
+#include <zmk/events/endpoint_changed.h>
+#include <zmk/events/ble_active_profile_changed.h>
 #include <zmk/split/bluetooth/peripheral.h>
 #include <zmk/usb.h>
+#include <zmk/ble.h>
+#include <zmk/endpoints.h>
 
 #include "animation.h"
 #include "battery.h"
 #include "output.h"
 #include "screen.h"
+#include "../assets/custom_fonts.h"
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
+
+/**
+ * Draw activity bar (bluetooth icon + battery percentage)
+ **/
+static void draw_activity_bar(lv_obj_t *canvas, const struct status_state *state) {
+    lv_draw_label_dsc_t label_dsc;
+    init_label_dsc(&label_dsc, LVGL_FOREGROUND, &pixel_operator_mono, LV_TEXT_ALIGN_LEFT);
+    
+    // Draw bluetooth/USB icon on the left
+    char icon_text[8] = {};
+    if (state->selected_endpoint.transport == 1) { // USB
+        strcpy(icon_text, "USB");
+    } else if (state->selected_endpoint.transport == 2) { // BLE
+        if (state->active_profile_connected) {
+            strcpy(icon_text, "BLE");
+        } else {
+            strcpy(icon_text, "---");
+        }
+    } else {
+        strcpy(icon_text, "N/A");
+    }
+    lv_canvas_draw_text(canvas, 0, 0, 30, &label_dsc, icon_text);
+    
+    // Draw battery percentage on the right
+    char battery_text[8] = {};
+    sprintf(battery_text, "%d%%", state->battery);
+    lv_canvas_draw_text(canvas, 35, 0, 30, &label_dsc, battery_text);
+}
 
 /**
  * Draw canvas
@@ -25,8 +58,8 @@ static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 static void draw_canvas(lv_obj_t *widget, lv_color_t cbuf[], const struct status_state *state) {
     lv_obj_t *canvas = lv_obj_get_child(widget, 0);
     lv_canvas_fill_bg(canvas, LVGL_BACKGROUND, LV_OPA_COVER);
-    // Main OLED: show only battery and connection status, no meters, no art/animation
-    draw_battery_status(canvas, state);
+    // Main OLED: show only activity bar with bluetooth icon and battery percentage
+    draw_activity_bar(canvas, state);
     rotate_canvas(canvas, cbuf);
 }
 
@@ -60,6 +93,40 @@ ZMK_SUBSCRIPTION(widget_battery_status, zmk_usb_conn_state_changed);
 #endif
 
 /**
+ * Output status (for bluetooth/USB icon)
+ **/
+static void set_output_status(struct zmk_widget_screen *widget, struct output_status_state state) {
+    widget->state.selected_endpoint = state.selected_endpoint;
+    widget->state.active_profile_index = state.active_profile_index;
+    widget->state.active_profile_connected = state.active_profile_connected;
+    widget->state.active_profile_bonded = state.active_profile_bonded;
+    draw_canvas(widget->obj, widget->cbuf, &widget->state);
+}
+
+static void main_output_status_update_cb(struct output_status_state state) {
+    struct zmk_widget_screen *widget;
+    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) { set_output_status(widget, state); }
+}
+
+static struct output_status_state main_output_status_get_state(const zmk_event_t *eh) {
+    return (struct output_status_state){
+        .selected_endpoint = zmk_endpoints_selected(),
+        .active_profile_index = zmk_ble_active_profile_index(),
+        .active_profile_connected = zmk_ble_active_profile_is_connected(),
+        .active_profile_bonded = !zmk_ble_active_profile_is_open(),
+    };
+}
+
+ZMK_DISPLAY_WIDGET_LISTENER(widget_main_output_status, struct output_status_state, main_output_status_update_cb, main_output_status_get_state)
+ZMK_SUBSCRIPTION(widget_main_output_status, zmk_endpoint_changed);
+#if IS_ENABLED(CONFIG_USB_DEVICE_STACK)
+ZMK_SUBSCRIPTION(widget_main_output_status, zmk_usb_conn_state_changed);
+#endif
+#if defined(CONFIG_ZMK_BLE)
+ZMK_SUBSCRIPTION(widget_main_output_status, zmk_ble_active_profile_changed);
+#endif
+
+/**
  * Peripheral status
  **/
 static struct peripheral_status_state get_state(const zmk_event_t *_eh) {
@@ -90,12 +157,12 @@ int zmk_widget_screen_init(struct zmk_widget_screen *widget, lv_obj_t *parent) {
     lv_obj_set_size(widget->obj, CANVAS_HEIGHT, CANVAS_WIDTH);
     lv_obj_t *canvas = lv_canvas_create(widget->obj);
     lv_obj_align(canvas, LV_ALIGN_TOP_LEFT, 0, 0);
-    lv_canvas_set_buffer(canvas, widget->cbuf, CANVAS_HEIGHT, CANVAS_HEIGHT, LV_IMG_CF_TRUE_COLOR);
-    sys_slist_append(&widgets, &widget->node);
+    lv_canvas_set_buffer(canvas, widget->cbuf, CANVAS_HEIGHT, CANVAS_HEIGHT, LV_IMG_CF_TRUE_COLOR);    sys_slist_append(&widgets, &widget->node);
 #if defined(draw_animation)
     draw_animation(canvas, widget);
 #endif
     widget_battery_status_init();
+    widget_main_output_status_init();
     widget_peripheral_status_init();
     return 0;
 }
