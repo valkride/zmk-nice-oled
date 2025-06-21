d9004ace00c084de3c0bc17d99a6bade6908a2d2
Valkride <venzovossen@outlook.com>
2025-06-22 01:10:27 +0200

    modified:   boards/shields/nice_oled/widgets/util.h

--- boards/shields/nice_oled/widgets/util.h
+++ boards/shields/nice_oled/widgets/util.h
@@ -16,7 +16,8 @@
 struct status_state {
   uint8_t battery;
   bool charging;
-#if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)  struct zmk_endpoint_instance selected_endpoint;
+#if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
+  struct zmk_endpoint_instance selected_endpoint;
   int active_profile_index;
   bool active_profile_connected;
   bool active_profile_bonded;
