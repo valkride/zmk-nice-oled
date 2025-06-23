fb19fdf5a4d914bc46adc2bbe9b335cea1b3bc65
Valkride <venzovossen@outlook.com>
2025-06-23 18:56:10 +0200

    modified:   boards/shields/nice_oled/widgets/host-data.c

--- boards/shields/nice_oled/widgets/host-data.c
+++ boards/shields/nice_oled/widgets/host-data.c
@@ -4,12 +4,9 @@
 
 void draw_host_data_status(lv_obj_t *canvas, const struct status_state *state) {
     lv_draw_label_dsc_t label_dsc;
-    init_label_dsc(&label_dsc, LVGL_FOREGROUND, &pixel_operator_mono, LV_TEXT_ALIGN_LEFT);
-
-    char text[16] = {};
+    init_label_dsc(&label_dsc, LVGL_FOREGROUND, &pixel_operator_mono_8, LV_TEXT_ALIGN_LEFT);    char text[16] = {};
     snprintf(text, sizeof(text), "GPU:100%%");
     
-    // Position the text between the separator lines
-    // Try y=80 to be closer to the first line and more visible
-    lv_canvas_draw_text(canvas, 0, 80, 68, &label_dsc, text);
+    // Position the text higher, closer to the top separator line
+    lv_canvas_draw_text(canvas, 0, 72, 68, &label_dsc, text);
 }
