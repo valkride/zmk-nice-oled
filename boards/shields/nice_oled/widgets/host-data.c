d12d3d6ddcf52e8afa9022cf6319894a472854bf
Valkride <venzovossen@outlook.com>
2025-06-23 19:03:24 +0200

    modified:   boards/shields/nice_oled/widgets/host-data.c

--- boards/shields/nice_oled/widgets/host-data.c
+++ boards/shields/nice_oled/widgets/host-data.c
@@ -5,7 +5,13 @@
 void draw_host_data_status(lv_obj_t *canvas, const struct status_state *state) {
     lv_draw_label_dsc_t label_dsc;
     init_label_dsc(&label_dsc, LVGL_FOREGROUND, &pixel_operator_mono_8, LV_TEXT_ALIGN_LEFT);    char text[16] = {};
-    snprintf(text, sizeof(text), "GPU:100%%");
+    snprintf(text, sizeof(text), "28/12/2023");
+    snprintf(text, sizeof(text), "14:30");
+    snprintf(text, sizeof(text), "CPU:100");
+    snprintf(text, sizeof(text), "GPU:100");
+    snprintf(text, sizeof(text), "RAM:100");
+    snprintf(text, sizeof(text), "DSK:100");
+
     
     // Position the text higher, closer to the top separator line
     lv_canvas_draw_text(canvas, 0, 72, 68, &label_dsc, text);
