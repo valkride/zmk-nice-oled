6da3ad77f448d107a71f08897b419cf47e7cb4f4
Valkride <venzovossen@outlook.com>
2025-06-23 19:41:46 +0200

    modified:   boards/shields/nice_oled/widgets/host-data.c

--- boards/shields/nice_oled/widgets/host-data.c
+++ boards/shields/nice_oled/widgets/host-data.c
@@ -15,22 +15,20 @@ void draw_host_data_status(lv_obj_t *canvas, const struct status_state *state) {
     char time_text[16] = {};
     snprintf(time_text, sizeof(time_text), "14:30");
     lv_canvas_draw_text(canvas, 15, 78, 50, &label_dsc, time_text);
-    
-    // Draw system info with shorter text to fit
+      // Draw system info with shorter text to fit
     char cpu_text[16] = {};
     snprintf(cpu_text, sizeof(cpu_text), "CPU:100");
-    lv_canvas_draw_text(canvas, 0, 88, 68, &label_left_dsc, cpu_text);
+    lv_canvas_draw_text(canvas, 0, 88, 68, &label_dsc, cpu_text);
     
     char gpu_text[16] = {};
     snprintf(gpu_text, sizeof(gpu_text), "GPU:100");
-    lv_canvas_draw_text(canvas, 0, 98, 68, &label_left_dsc, gpu_text);
+    lv_canvas_draw_text(canvas, 0, 98, 68, &label_dsc, gpu_text);
     
     char ram_text[16] = {};
     snprintf(ram_text, sizeof(ram_text), "RAM:100");
-    lv_canvas_draw_text(canvas, 0, 108, 68, &label_left_dsc, ram_text);
-    
+    lv_canvas_draw_text(canvas, 0, 108, 68, &label_dsc, ram_text);    
     char dsk_text[16] = {};
     snprintf(dsk_text, sizeof(dsk_text), "DSK:100");
-    lv_canvas_draw_text(canvas, 0, 118, 68, &label_left_dsc, dsk_text);
+    lv_canvas_draw_text(canvas, 0, 118, 68, &label_dsc, dsk_text);
     */
 }
