e01ad653bbdc3fd3c385b929cb1a97f9b31b37fd
Valkride <venzovossen@outlook.com>
2025-06-24 14:43:18 +0200

    modified:   boards/shields/nice_oled/widgets/host-data.c

--- boards/shields/nice_oled/widgets/host-data.c
+++ boards/shields/nice_oled/widgets/host-data.c
@@ -86,25 +86,24 @@ static void parse_hid_data(uint8_t *data, uint8_t length) {
 }
 
 void draw_host_data_status(lv_obj_t *canvas, const struct status_state *state) {
-    lv_draw_label_dsc_t label_dsc;
-    init_label_dsc(&label_dsc, LVGL_FOREGROUND, &pixel_operator_mono_12, LV_TEXT_ALIGN_LEFT);
+    lv_draw_label_dsc_t label_dsc;    init_label_dsc(&label_dsc, LVGL_FOREGROUND, &pixel_operator_mono_12, LV_TEXT_ALIGN_LEFT);
     
-    // Draw system info with full indicator names
+    // Draw system info with better spacing and alignment
     char cpu_text[16] = {};
-    snprintf(cpu_text, sizeof(cpu_text), "C:%s%%", g_cpu);
-    lv_canvas_draw_text(canvas, 0, 68, 50, &label_dsc, cpu_text);
+    snprintf(cpu_text, sizeof(cpu_text), "CPU %2s%%", g_cpu);
+    lv_canvas_draw_text(canvas, 0, 68, 60, &label_dsc, cpu_text);
+    
+    char ram_text[16] = {};
+    snprintf(ram_text, sizeof(ram_text), "RAM %2s%%", g_ram);
+    lv_canvas_draw_text(canvas, 0, 78, 60, &label_dsc, ram_text);
     
     char gpu_text[16] = {};
-    snprintf(gpu_text, sizeof(gpu_text), "G:%s%%", g_gpu);
-    lv_canvas_draw_text(canvas, 0, 78, 50, &label_dsc, gpu_text);
+    snprintf(gpu_text, sizeof(gpu_text), "GPU %2s%%", g_gpu);
+    lv_canvas_draw_text(canvas, 0, 88, 60, &label_dsc, gpu_text);
 
-    char ram_text[16] = {};
-    snprintf(ram_text, sizeof(ram_text), "R:%s%%", g_ram);
-    lv_canvas_draw_text(canvas, 0, 88, 50, &label_dsc, ram_text);
-    
     char dsk_text[16] = {};
-    snprintf(dsk_text, sizeof(dsk_text), "D:%s%%", g_disk);
-    lv_canvas_draw_text(canvas, 0, 98, 50, &label_dsc, dsk_text);
+    snprintf(dsk_text, sizeof(dsk_text), "DSK %2s%%", g_disk);
+    lv_canvas_draw_text(canvas, 0, 98, 60, &label_dsc, dsk_text);
 }
 
 // Update all screen widgets when new HID data is received
